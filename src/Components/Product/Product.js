758cd980156c0985a5c5ee04fcf1052d4d3aa788
Ovijitnath <ovijitnathgn@gmail.com>
2022-09-29 02:19:49 +0600

    question answer added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Products.css'
 const Product = (props) => {
-    const { name, img, seller, price } = props.product;
+    const { name, img, age, time } = props.product;
     return (
         <div className='product'>
             <img src={img} alt=""></img>
             <div className='product-info'>
                 <h5 className='product-name'>{name}</h5>
-                <p>Price: ${price}</p>
-                <p><small>Seller: {seller}</small></p>
+                <p>Age: {age}</p>
+                <p>Time: {time}</p>
 
             </div>
             <button className='btn-cart'>
